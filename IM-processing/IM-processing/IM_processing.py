a3514e7883db8c21b861dcfc97f31c357f1835f5
Eber Lawrence <eberlawrence@hotmail.com>
2018-11-21 14:51:55 -0200

    Updated.

--- IM-processing/IM-processing/IM_processing.py
+++ IM-processing/IM-processing/IM_processing.py
@@ -1,1 +1,9 @@
+import pyedflib
+import numpy as np
 
+f = pyedflib.EdfReader("C:\\Users\\BioLab\\Desktop\\GitHub\\Motor-imagery-processing\\EEG_coleta\\FHILLIPE-E.edf")
+n = f.signals_in_file
+signal_labels = f.getSignalLabels()
+sigbufs = np.zeros((n, f.getNSamples()[0]))
+for i in np.arange(n):
+    sigbufs[i, :] = f.readSignal(i)
